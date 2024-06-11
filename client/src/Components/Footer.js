65761af2bdf8b726fd6e509a8e7da095b0803647
Bastien <bastien.youssfi@gmail.com>
2024-06-11 09:23:41 +0900

    Register and Login page looking better

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="text-white font-mono text-center mt-8">
-            <button className="mb-2 text-white text-2xl hover:underline">Contact me!</button>
+        <footer className="text-white font-mono text-center mt-4">
+            <a className="mb-4 text-white text-2xl hover:underline" href="http://linkedin.com/in/bastienyoussfi">Contact me!</a>
             <p>© 2024 BY' - All rights reserved</p>
         </footer>
     )
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -34,10 +34,10 @@ export default function RegisterPage() {
     }
 
     return (
-        <div className="w-full max-w-[400px] mx-[auto] my-[0]">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="w-full mt-48 max-w-[400px] mx-[auto] my-[0] font-mono">
+            <form className="bg-[#1d1d1d] shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                    <label className="block text-white text-sm mb-2" htmlFor="username">
                         Username
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
@@ -48,7 +48,7 @@ export default function RegisterPage() {
                     />
                 </div>
                 <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    <label className="block text-white text-sm mb-2" htmlFor="password">
                         Password
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
@@ -59,7 +59,7 @@ export default function RegisterPage() {
                     />
                 </div>
                 <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    <label className="block text-white text-sm mb-2" htmlFor="password">
                         Confirm password
                     </label>
                     <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
@@ -71,7 +71,7 @@ export default function RegisterPage() {
                     <p className="text-red-500 text-xs italic">Please choose a password.</p>
                 </div>
                 <div className="flex items-center justify-center">
-                    <button onClick={ register } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <button onClick={ register } className="hover:underline text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                         Register
                     </button>
                 </div>
